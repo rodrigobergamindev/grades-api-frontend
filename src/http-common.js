d0b54f458a80397936b6d9f1225a3d1f9b8f3907
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2020-07-06 13:26:33 -0300

    commit

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grades-api-backend.herokuapp.com/',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Content-type': 'application/json',
   },
