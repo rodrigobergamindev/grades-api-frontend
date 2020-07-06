0b83939777f04c5a0e26a2da5fd8538d23d3f045
Rodrigo Bergamin <rodrigobergamindev@gmail.com>
2020-07-06 11:46:43 -0300

    commit

--- src/http-common.js
+++ src/http-common.js
@@ -2,8 +2,5 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: process.env.API_URL,
-  headers: {
-    'Content-type': 'application/json',
-  },
+  baseURL: process.env.API_URL
 });
